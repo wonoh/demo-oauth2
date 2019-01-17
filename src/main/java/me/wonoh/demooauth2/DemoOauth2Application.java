4714b14bf5fa89f9e24d90912cde5592a3b0c43f
wonoh <wldnjsdh44121@nate.com>
2019-01-17 14:37:41 +0900

    Initial commit

---
+++ src/main/java/me/wonoh/demooauth2/DemoOauth2Application.java
@@ -0,0 +1,14 @@
+package me.wonoh.demooauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoOauth2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoOauth2Application.class, args);
+    }
+
+}
+
