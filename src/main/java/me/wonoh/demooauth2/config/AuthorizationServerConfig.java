0bbcbd2ae2a85fbac8ede36bf115071e173b17dc
wonoh <wldnjsdh44121@nate.com>
2019-01-17 16:01:07 +0900

    인증 서버 설정

--- src/main/java/me/wonoh/demooauth2/config/AuthorizationServerConfig.java
+++ src/main/java/me/wonoh/demooauth2/config/AuthorizationServerConfig.java
@@ -4,11 +4,15 @@ package me.wonoh.demooauth2.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
+
+import javax.annotation.Resource;
 
 @Configuration
 @EnableAuthorizationServer
@@ -19,12 +23,17 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     @Autowired
     private AuthenticationManager authenticationManager;
 
+    @Autowired
+    PasswordEncoder passwordEncoder;
+    @Resource(name = "userService")
+    UserDetailsService userDetailsService;
+
     @Override
     public void configure(ClientDetailsServiceConfigurer configurer) throws Exception {
         configurer
                 .inMemory()
                 .withClient("wonoh-client")
-                .secret("wonoh-password")  // id , secret 인증에 필요한 정보!
+                .secret(passwordEncoder.encode("wonoh-password"))  // id , secret 인증에 필요한 정보!
                 .authorizedGrantTypes("password",
                         "authorization_code",
                         "refresh_token",
@@ -37,6 +46,7 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
         endpoints.tokenStore(tokenStore)
-                .authenticationManager(authenticationManager);
+                .authenticationManager(authenticationManager)
+                .userDetailsService(userDetailsService);
     }
 }
